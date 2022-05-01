fac5358b1e4079f82b84b52d5d8ac4fdd7a8c9c3
rodrigosantosdev <rodrigofrontend@gmail.com>
2022-05-01 15:48:26 -0300

    a

--- Filtro/app.js
+++ Filtro/app.js
@@ -1,14 +1,82 @@
-const carros = [
-  { id: 1, name: 'Fusca', marca: 'vols', price: 10.00 },
-  { id: 2, name: 'Gol', marca: 'vols', price: 36.00 },
-  { id: 3, name: 'Creta', marca: 'hyuudai', price: 100.00 },
-  { id: 4, name: '308', marca: 'pegeout', price: 225.00 },
-  { id: 5, name: 'RS8', marca: 'audi', price: 145.00 }
-]
+let filteredProducts = [...products];
 
-const inputSearch = document.querySelector('#inputTxt')
-console.log(inputSearch)
+const productsContainer = document.querySelector('.products-container');
 
-inputSearch.addEventListener('input', event => {
-  console.log(event.target.value.trim())
-})
+const displayProducts = () => {
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
+    return;
+  }
+
+  productsContainer.innerHTML = filteredProducts
+    .map((product) => {
+      const { id, title, image, price } = product;
+      return `<article class="product" data-id="${id}">
+          <img
+            src="${image}"
+            class="product-img img"
+            alt=""
+          />
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">${price}</span>
+          </footer>
+        </article>`;
+    })
+    .join('');
+};
+
+displayProducts();
+
+// Text Filter
+
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+form.addEventListener('keyup', () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
+
+// console.log(
+//   products.filter((product) => {
+//     return product.title.toLowerCase().includes('');
+//   })
+// );
+
+// Filter Buttons
+
+const companiesDOM = document.querySelector('.companies');
+
+const displayButtons = () => {
+  const buttons = [
+    'all',
+    ...new Set(products.map((product) => product.company)),
+  ];
+  // console.log(buttons);
+  companiesDOM.innerHTML = buttons
+    .map((company) => {
+      return `<button class='company-btn' data-id="${company}">${company}</button>`;
+    })
+    .join('');
+};
+
+displayButtons();
+
+companiesDOM.addEventListener('click', (e) => {
+  const el = e.target;
+  if (el.classList.contains('company-btn')) {
+    if (el.dataset.id === 'all') {
+      filteredProducts = [...products];
+    } else {
+      filteredProducts = products.filter((product) => {
+        return product.company === el.dataset.id;
+      });
+    }
+    searchInput.value = '';
+    displayProducts();
+  }
+});
